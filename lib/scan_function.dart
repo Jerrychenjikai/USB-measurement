f2a8607bd917aee0f62686773932f01e72528886
Jerrychenjikai <jerry_chenjikai@hotmail.com>
2026-06-09 23:28:22 +0800

    windows build

--- lib/scan_function.dart
+++ lib/scan_function.dart
@@ -5,11 +5,11 @@ import 'dart:io' show Platform;
 // 如果编译 Android，请注释掉 desktop，打开 android：
 // export 'android_basic_func.dart';
 
-export 'android_basic_func.dart';
+export 'desktop_basic_func.dart';
 // =======================================================
 
 // 依然提供这个函数名，供外部（如主页）调用，内部直接代理给底层函数
-import 'android_basic_func.dart'; // 这里的引入需要和上面 export 保持一致
+import 'desktop_basic_func.dart'; // 这里的引入需要和上面 export 保持一致
 
 Future<List<MySerialDevice>> getAvailablePorts() async {
   // 直接调用我们在底层文件中封装好的同名函数
